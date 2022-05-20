34eb9a1866663d2c6750d78ae161dab14cc41700
Carlos Flores <c@mirror.xyz>
2022-05-20 10:20:07 -0400

    update pass

--- src/NFTBridgePass.sol
+++ src/NFTBridgePass.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "openzeppelin-contracts/contracts/utils/Base64.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
@@ -31,10 +32,28 @@ interface INFTBridgePass {
 contract NFTBridgePass is IERC721Metadata, ERC721, INFTBridgePass {
     address public immutable override l2TokenBridge;
 
+    uint256 public immutable limit;
+
+    string public baseURI;
+
+    string public description;
+
+    string public imageURI;
+
     uint256 public totalSupply = 0;
 
-    constructor(address _l2TokenBridge) ERC721("NFT Bridge Pass", "NFTPASS") {
+    constructor(
+        address _l2TokenBridge,
+        uint256 _limit,
+        string memory _baseURI,
+        string memory _description,
+        string memory _imageURI
+    ) ERC721("NFT Bridge Pass", "NFTPASS") {
         l2TokenBridge = _l2TokenBridge;
+        limit = _limit;
+        baseURI = _baseURI;
+        description = _description;
+        imageURI = _imageURI;
     }
 
     function tokenURI(uint256 tokenId)
@@ -43,7 +62,31 @@ contract NFTBridgePass is IERC721Metadata, ERC721, INFTBridgePass {
         override(ERC721, IERC721Metadata)
         returns (string memory)
     {
-        return Strings.toString(tokenId);
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            string(
+                                abi.encodePacked(
+                                    '{"name": "',
+                                    name(),
+                                    '", "description": "',
+                                    description,
+                                    '", "animation_uri": "',
+                                    _constructURI(tokenId, ownerOf(tokenId)),
+                                    '", image: "',
+                                    abi.encodePacked(imageURI),
+                                    '", "attributes":[{ "trait_type": "Serial", "value": ',
+                                    Strings.toString(tokenId),
+                                    "}] }"
+                                )
+                            )
+                        )
+                    )
+                )
+            );
     }
 
     function mint(address to, uint32 _l2Gas)
@@ -51,6 +94,8 @@ contract NFTBridgePass is IERC721Metadata, ERC721, INFTBridgePass {
         payable
         returns (uint256 tokenId)
     {
+        require(totalSupply < limit, "sold out");
+
         tokenId = ++totalSupply;
 
         _mint(to, tokenId);
@@ -60,5 +105,42 @@ contract NFTBridgePass is IERC721Metadata, ERC721, INFTBridgePass {
             _l2Gas,
             ""
         );
+    }
+
+    function _constructURI(uint256 tokenId, address owner)
+        internal
+        view
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    baseURI,
+                    "?totalTickets=",
+                    Strings.toString(limit),
+                    "&ticketNumber=",
+                    Strings.toString(tokenId),
+                    "&ownerAddress=",
+                    _addressToString(owner)
+                )
+            );
+    }
+
+    // https://ethereum.stackexchange.com/questions/8346/convert-address-to-string/8447#8447
+    function _addressToString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint256 i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8 * (19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2 * i] = _char(hi);
+            s[2 * i + 1] = _char(lo);
+        }
+        return string(abi.encodePacked("0x", s));
+    }
+
+    function _char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
     }
 }
--- src/scripts/NFTBridgePassScript.sol
+++ src/scripts/NFTBridgePassScript.sol
@@ -14,14 +14,20 @@ contract NFTBridgePassScript {
         Vm(address(bytes20(uint160(uint256(keccak256("hevm cheat code"))))));
 
     function run() external {
-        vm.deal(address(this), 1 ether);
+        vm.deal(address(this), 2 ether);
 
         // Optimism's Kovan Bridge address
         address l2BridgeToken = address(
             0x22F24361D548e5FaAfb36d1437839f080363982B
         );
 
-        NFTBridgePass nftBridgePass = new NFTBridgePass(l2BridgeToken);
+        NFTBridgePass nftBridgePass = new NFTBridgePass(
+            l2BridgeToken,
+            2,
+            "https://l2-invite.vercel.app/?",
+            "devs do something?",
+            "ipfs://bafkreihitm7yae6xkhsdgi5ekkhxmc6vsls3kcz7jr72vzggjq5jr2hm3a"
+        );
 
         uint32 l2Gas = 1300000;
 
@@ -32,10 +38,20 @@ contract NFTBridgePassScript {
 
         console.logString("tokenId:");
         console.logUint(tokenId);
+        console.logString("tokenURI:");
+        console.logString(nftBridgePass.tokenURI(1));
 
         console.logString("name:");
         console.logString(nftBridgePass.name());
         console.logString("symbol:");
         console.logString(nftBridgePass.symbol());
+        console.logString("symbol:");
+        console.logString(nftBridgePass.symbol());
+
+        tokenId = nftBridgePass.mint{value: 1 ether}(address(this), l2Gas);
+        console.logString("tokenId:");
+        console.logUint(tokenId);
+        console.logString("tokenURI:");
+        console.logString(nftBridgePass.tokenURI(1));
     }
 }
